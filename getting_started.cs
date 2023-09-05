0d306a570ff04e6ebf12e7a7e45c0f988e839fa0
27tr <114559221+MCDream3@users.noreply.github.com>
2023-09-05 11:17:18 +0100

    Create

---
+++ getting_started.cs
@@ -0,0 +1,19 @@
+//                                                                              <<command prompt>>
+//dotnet --version              <--versja dotnet-a
+//dotnet --info                 <-- informacja
+//dotnet new                    
+//{dotnet new console project >>> dotnet new console -o project      <--nowy projekt
+
+//dir >>> tree >>>  tree /f          <--zawartość
+//dotnet new console
+
+//                                                                                <<code>>
+Console.WriteLine("Hello World!");
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
